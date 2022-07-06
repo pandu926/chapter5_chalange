518a3cdaab1fecd7f1a03584612cfd4f158b61f1
pandu926 <63030609+pandu926@users.noreply.github.com>
2022-07-06 21:48:50 +0700

    resolved com logic in gamesuit

--- assets/chapter4/app.js
+++ assets/chapter4/app.js
@@ -11,52 +11,58 @@ let comChange = document.getElementsByClassName(com);
 
 // dom event untuk pemilihan
 
-batu.onclick = function () {
-  batu.style.background = "white";
-  let player = "batu";
-  let gameSuit = new GameSuit(com, player);
-  comChange[0].style.background = "white";
-  result.style.opacity = 1;
-  let hasil = gameSuit.rules();
-  resultText.innerHTML = hasil;
+batu.onclick = function() {
+    com = comSelect();
+    comChange = document.getElementsByClassName(com);
+    batu.style.background = "white";
+    let player = "batu";
+    let gameSuit = new GameSuit(com, player);
+    comChange[0].style.background = "white";
+    result.style.opacity = 1;
+    let hasil = gameSuit.rules();
+    resultText.innerHTML = hasil;
 };
 
-gunting.onclick = function () {
-  gunting.style.background = "white";
-  let player = "gunting";
-  let gameSuit = new GameSuit(com, player);
-  comChange[0].style.background = "white";
-  result.style.opacity = 1;
-  let hasil = gameSuit.rules();
-  resultText.innerHTML = hasil;
+gunting.onclick = function() {
+    com = comSelect();
+    comChange = document.getElementsByClassName(com);
+    gunting.style.background = "white";
+    let player = "gunting";
+    let gameSuit = new GameSuit(com, player);
+    comChange[0].style.background = "white";
+    result.style.opacity = 1;
+    let hasil = gameSuit.rules();
+    resultText.innerHTML = hasil;
 };
-kertas.onclick = function () {
-  kertas.style.background = "white";
-  let player = "kertas";
-  let gameSuit = new GameSuit(com, player);
-  comChange[0].style.background = "white";
-  result.style.opacity = 1;
-  let hasil = gameSuit.rules();
-  resultText.innerHTML = hasil;
+kertas.onclick = function() {
+    com = comSelect();
+    comChange = document.getElementsByClassName(com);
+    kertas.style.background = "white";
+    let player = "kertas";
+    let gameSuit = new GameSuit(com, player);
+    comChange[0].style.background = "white";
+    result.style.opacity = 1;
+    let hasil = gameSuit.rules();
+    resultText.innerHTML = hasil;
 };
-reset.onclick = function () {
-  gunting.style.background = "none";
-  batu.style.background = "none";
-  kertas.style.background = "none";
-  comChange[0].style.background = "none";
-  result.style.opacity = 0;
+reset.onclick = function() {
+    gunting.style.background = "none";
+    batu.style.background = "none";
+    kertas.style.background = "none";
+    comChange[0].style.background = "none";
+    result.style.opacity = 0;
 };
 
 // menentukan pilihan computer
 
 function comSelect() {
-  let com = Math.random();
-  if (com < 0.34) {
-    com = "batu";
-  } else if (com > 0.33 && com < 0.67) {
-    com = "kertas";
-  } else {
-    com = "gunting";
-  }
-  return com;
+    let com = Math.random();
+    if (com < 0.34) {
+        com = "batu";
+    } else if (com > 0.33 && com < 0.67) {
+        com = "kertas";
+    } else {
+        com = "gunting";
+    }
+    return com;
 }
